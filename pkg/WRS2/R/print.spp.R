b95054b8d717cf89c4cba54c9472c8416802d37d
pmair <pmair@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-06 16:09:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/psychor@231 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WRS2/R/print.spp.R
+++ pkg/WRS2/R/print.spp.R
@@ -7,7 +7,7 @@ function(x,...)
   cat("\nTest statistics:\n")
   #cat(round(x$test, 4), "\n")
   print(x$test, digits = 4)
-  cat("\nTest whether all the population parameters of the above estimates are the same:\n")
+  cat("\nTest whether the corrresponding population parameters are the same:\n")
   cat("p-value:", round(x$p.value, 5), "\n")
   cat("\n")
 }
